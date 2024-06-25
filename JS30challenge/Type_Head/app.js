b331bf6d6ebe1cab5a6fb03d8196d8367f3fa12c
Tanu Lamba <tanulamba2003@gmail.com>
2024-06-25 13:03:39 +0530

    type ahead

---
+++ JS30challenge/Type_Head/app.js
@@ -0,0 +1,42 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+let searchInput = document.querySelector(".search");
+let suggestions = document.querySelector(".suggestions");
+
+const cities = [];
+
+fetch(endpoint)
+    .then(data => data.json())
+    .then(jsonData => cities.push(...jsonData));
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+
+        const regex = new RegExp(wordToMatch, "gi");
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function displayMatches() {
+    const matchArr = findMatches(this.value, cities);
+    const html = matchArr.map(place => {
+        const regex = new RegExp(this.value, "gi");
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+        <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+        </li>
+        `;
+
+    }).join("");
+    suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
