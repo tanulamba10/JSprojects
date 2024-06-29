3408ed58dd274c93b5ac47194d386286078254f5
Tanu Lamba <tanulamba2003@gmail.com>
2024-06-29 18:19:27 +0530

    array cardio 2

---
+++ JS30challenge/html5Canvas/app.js
@@ -0,0 +1,54 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+ctx.strokeStyle = "green";
+ctx.lineJoin = 'round';
+ctx.lineCap = "round";
+ctx.lineWidth = 40;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+    if (!isDrawing) return;
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.x, e.y);
+    ctx.stroke();
+    [lastX, lastY] = [e.x, e.y];
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 70 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+}
+
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.x, e.y];
+
+});
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseout", () => isDrawing = false);
