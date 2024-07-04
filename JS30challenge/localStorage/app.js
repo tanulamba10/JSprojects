8e8084cd9448ffce8ddeaf8497189dd429d57e44
Tanu Lamba <tanulamba2003@gmail.com>
2024-07-04 10:28:10 +0530

    delete and clear button

--- JS30challenge/localStorage/app.js
+++ JS30challenge/localStorage/app.js
@@ -1,6 +1,7 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem("items")) || [];
+const clearBtn = document.querySelector(".clear");
 
 function addItem(e) {
     e.preventDefault();
@@ -23,6 +24,7 @@ function populateList(plates = [], plateList) {
         <li>
         <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? "checked" : ''}>
         <label for="item${i}">${plate.text}</label>
+        <button class="active" data-index=${i}>Delete</button>
         </li>
         `;
     }).join('');
@@ -38,8 +40,29 @@ function toggleDone(e) {
     populateList(items, itemsList);
 }
 
+function deleteItem(e) {
+    if (!e.target.matches("button")) return;
+    let index = e.target.dataset;
+    items.pop(items[index]);
+    localStorage.setItem("items", JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+function clearAll(e) {
+    items.forEach((item) => {
+        item.done = false;
+    });
+    localStorage.setItem("items", JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+
 itemsList.addEventListener("click", toggleDone);
+itemsList.addEventListener("click", deleteItem);
 
 addItems.addEventListener("submit", addItem);
 
+clearBtn.addEventListener("click", clearAll);
+
 populateList(items, itemsList);
+
